a9ba5f29d991da74b92aa5191694b4b6629876c1
vitaliklevcenko <vitalik72004@gmail.com>
2023-03-29 13:16:28 +0200

    Створенно функцію яка зчитує вміст файлів

--- main.py
+++ main.py
@@ -2,3 +2,10 @@
 
 f1 = open('file1.txt', 'r')
 f2 = open('file2.txt', 'r')
+
+def readdata(f):
+    # зчитуємо вміст файлів
+    f_lines = []
+    for line in f:
+        f_lines.append(line)
+    return f_lines
